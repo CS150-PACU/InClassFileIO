40b7ddca55596e5b16a700390b2b0538e69a3f44
chadd williams <will4614@pacificu.edu>
2022-10-01 22:36:26 -0700

    Add project files.

---
+++ InClassFileIO/main.cpp
@@ -0,0 +1,20 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				chadd williams
+// Date:					Oct 1, 2022
+// Class:					CS 150-02
+// Assignment:		In Class File Examples
+// Purpose:				File IO
+// Hours Worked:	50 minutes
+//*********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+	return EXIT_SUCCESS;
+}
